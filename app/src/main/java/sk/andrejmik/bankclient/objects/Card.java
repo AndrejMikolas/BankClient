1dc2bf46972bbd18d74a6a8ecc3889d68882fa60
Andrej Mikoláš <andrej.mikolas@gmail.com>
2020-05-24 20:20:10 +0200

    Add row UI for cards list

--- app/src/main/java/sk/andrejmik/bankclient/objects/Card.java
+++ app/src/main/java/sk/andrejmik/bankclient/objects/Card.java
@@ -10,8 +10,18 @@ import lombok.Setter;
 public class Card extends BaseEntity
 {
     private String cardNo;
-
+    
     private int expirationYear;
-
+    
     private int expirationMonth;
+    
+    public String getCardNoFormatted()
+    {
+        if (cardNo == null)
+        {
+            return null;
+        }
+        char delimiter = ' ';
+        return cardNo.replaceAll(".{4}(?!$)", "$0" + delimiter);
+    }
 }
