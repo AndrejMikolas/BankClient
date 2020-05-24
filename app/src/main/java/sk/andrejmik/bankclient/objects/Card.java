71b27ad1d6417104e4e7dcc44900c2ff3933ab0e
Andrej Mikoláš <andrej.mikolas@gmail.com>
2020-05-25 00:33:27 +0200

    Add card expiration check to account detail

--- app/src/main/java/sk/andrejmik/bankclient/objects/Card.java
+++ app/src/main/java/sk/andrejmik/bankclient/objects/Card.java
@@ -1,5 +1,10 @@
 package sk.andrejmik.bankclient.objects;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,5 +28,22 @@ public class Card extends BaseEntity
         }
         char delimiter = ' ';
         return cardNo.replaceAll(".{4}(?!$)", "$0" + delimiter);
+    }
+    
+    public boolean getIsExpired()
+    {
+        String input = String.format(Locale.getDefault(), "%02d/%02d", expirationMonth+1, expirationYear);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/yy", Locale.getDefault());
+        simpleDateFormat.setLenient(false);
+        Date expiry = null;
+        try
+        {
+            expiry = simpleDateFormat.parse(input);
+        } catch (ParseException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+        return expiry.before(new Date());
     }
 }
